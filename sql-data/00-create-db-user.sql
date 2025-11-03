e6df4df3056bd5d0a7ac589d61cab190649a3aa6
matthcol <matthias.colin@gmail.com>
2025-11-03 10:06:29 +0100

    dbmovie scripts

---
+++ sql-data/00-create-db-user.sql
@@ -0,0 +1,10 @@
+-- execute this script in admin mode
+-- NB: you can create a specific database or work with db postgres
+create user movie with login password 'password';
+create database dbmovie encoding 'UTF-8';
+
+-- change database to create schema movie
+\c dbmovie
+create schema movie authorization movie;
+
+-- play other scripts 01-06 with user movie
